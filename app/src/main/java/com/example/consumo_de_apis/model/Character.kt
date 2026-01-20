a6eef289ce58816c55e31f1ee9abdb395932c48d
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2026-01-20 23:05:47 +0100

    refactor: cambio de modelo perro a modelo personage

--- app/src/main/java/com/example/consumo_de_apis/model/Character.kt
+++ app/src/main/java/com/example/consumo_de_apis/model/Character.kt
@@ -1,5 +1,14 @@
 package com.example.consumo_de_apis.model
 
-data class Character (
-    var name: String
+import androidx.annotation.DrawableRes
+import java.util.Date
+
+data class Personage (
+    var name: String,
+    var occupation: String,
+    var gender: String,
+    var birthday: Date,
+    var status: String,
+    var description: String,
+    @DrawableRes var image: Int
 )
