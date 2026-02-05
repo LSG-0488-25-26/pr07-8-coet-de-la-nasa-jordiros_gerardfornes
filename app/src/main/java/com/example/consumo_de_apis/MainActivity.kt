7c152149aa4e605ea7c9e61df5f69eda86175c9e
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2026-02-05 21:12:01 +0100

    docs: comentarios en MainActivity

--- app/src/main/java/com/example/consumo_de_apis/MainActivity.kt
+++ app/src/main/java/com/example/consumo_de_apis/MainActivity.kt
@@ -10,19 +10,26 @@ import com.example.consumo_de_apis.ui.theme.Consumo_de_apisTheme
 import com.example.consumo_de_apis.view.ScaffoldView
 import com.example.consumo_de_apis.viewmodel.ConsumoViewModel
 import com.example.consumo_de_apis.viewmodel.SearchBarViewModel
-import kotlin.getValue
 
+// ACTIVIDAD PRINCIPAL DE LA APLICACION
 class MainActivity : ComponentActivity() {
-    val consumoViewModel: ConsumoViewModel by viewModels<ConsumoViewModel>()
+    val consumoViewModel: ConsumoViewModel by viewModels<ConsumoViewModel>() // VIEWMODEL PRINCIPAL PARA CONSUMO DE API Y LOGICA GENERAL
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val searchBarViewModel by viewModels<SearchBarViewModel>()
+        val searchBarViewModel by viewModels<SearchBarViewModel>()// VIEWMODEL PARA LA SEARCH BAR
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Consumo_de_apisTheme {
-                val navController = rememberNavController()
-                ScaffoldView(consumoViewModel, navController, searchBarViewModel)
+                val navController = rememberNavController() // CONTROLADOR DE NAVEGACION ENTRE PANTALLAS
+
+                // VIEW PRINCIPAL QUE CONTIENE EL SCAFFOLD Y LA NAVEGACION
+                ScaffoldView(
+                    consumoViewModel,
+                    navController,
+                    searchBarViewModel
+                )
             }
         }
     }
